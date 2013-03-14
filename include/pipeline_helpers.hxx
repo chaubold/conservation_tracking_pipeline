c236e9cd745ecff1484b6f33ea224578c75b7ec3
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-03-14 13:28:05 +0100

    order of features?

--- include/pipeline_helpers.hxx
+++ include/pipeline_helpers.hxx
@@ -15,35 +15,106 @@
 // vigra
 #include <vigra/multi_array.hxx>
 #include <vigra/convolution.hxx>
+#include <vigra/multi_convolution.hxx>
+#include <vigra/tinyvector.hxx>
+#include <vigra/functorexpression.hxx>
+#include <vigra/multi_tensorutilities.hxx>
 
 // boost
 #include <boost/tokenizer.hpp>
 
 
 typedef vigra::MultiArray<2, double> feature_image;
+typedef float FEATURETYPE;
 
 
 // ArgumentError class
+class ArgumentError;
+
+
+// strip trailing slash
+// works only for null terminated cstrings
+void rstrip(char* c, char r);
+
+
+// remap image to [0,255]
+template <typename T, int N>
+void remap(vigra::MultiArray<N, T>& src);
+
+
+// zero padding int to string
+std::string zero_padding(int num, int n_zeros);
+
+
+// read features from csv_style file
+int read_features_from_file(std::string path, std::vector<std::pair<std::string, std::string> >& features);
+
+
+// lookup the size of given feature
+int feature_dim_lookup_size(std::string feature);
+
+
+// ilastik pixelclassification style difference of gaussians
+template <int N>
+void differenceOfGaussians(const vigra::MultiArray<N, unsigned>& src, vigra::MultiArray<N, FEATURETYPE>& dest, double scale0, double scale1, vigra::ConvolutionOptions<N> opt);
+
+
+// gaussianGradientMagnitude for MultiArrays
+template <int N>
+void gaussianGradientMagnitudeOwn(const vigra::MultiArray<N, unsigned>& src, vigra::MultiArray<N, FEATURETYPE>& dest, double scale, vigra::ConvolutionOptions<N> opt);
+
+
+// structureTensorEigenvalues for vector<MultiArray> instead of MultiArray<N, TinyVector>
+template <int N>
+void structureTensorEigenValuesOwn(const vigra::MultiArray<N, unsigned>& src, std::vector<vigra::MultiArray<N, FEATURETYPE> >& dest, double inner_scale, double outer_scale, vigra::ConvolutionOptions<N> opt);
+
+
+// hessianOfGaussianEigenvalues for vector<MultiArray> instead of MultiArray<N, TinyVector>
+template <int N>
+void hessianOfGaussianEigenvaluesOwn(const vigra::MultiArray<N, unsigned>& src, std::vector<vigra::MultiArray<N, FEATURETYPE> >& dest, double scale, vigra::ConvolutionOptions<N> opt);
+
+
+// MultiArray<N, TinyVector<T, U> > to vector<MultiArray<N, T> >
+template <int N, class T, int U>
+void multi_array_of_tiny_vec_to_vec_of_multi_array(const vigra::MultiArray<N, vigra::TinyVector<T, U> >& src,
+						   std::vector<vigra::MultiArray<N, T> >& dest);
+
+
+// get features from string storing in vector of MultiArrays
+template <int N>
+int get_features(const vigra::MultiArray<N, unsigned>& src, std::vector<vigra::MultiArray<N, FEATURETYPE> >& dest, std::string feature, double scale);
+
+
+// get features from string storing in MultiArray with Dim+1
+template <int N>
+int get_features(const vigra::MultiArray<N, unsigned>& src, vigra::MultiArrayView<N+1, FEATURETYPE> dest, std::string feature, double scale, int pos_in_res_array, double window_size = 2.0);
+
+
+// extract number from string
+double string_to_double(std::string str);
+
+
+// read config from file (such as renormalization parameters,...)
+int read_config_from_file(const std::string& path, std::map<std::string, double>& options);
+
+
+// renormalize multiarray to 8bit
+template <int N, class T>
+void renormalize_to_8bit(vigra::MultiArrayView<N, T>& array, double min, double max);
+
+
+
+/* -------------------------------------------------- */
+/*                   IMPLEMENTATION                   */
+/* -------------------------------------------------- */
+
+
 class ArgumentError : public std::exception {
 public:
   virtual const char* what() const throw() {return "Dataset folder and seqeunce not specified!\n";}
 };
 
 
-// strip trailing slash
-// works only for null terminated cstrings
-void rstrip(char* c, char r) {
-  while (*c != '\0') {
-    if (*(c+1) == '\0' && *c == r) {
-      *c = '\0';
-    }
-    c++;
-  }
-  return;
-}
-
-
-// remap image to [0,255]
 template <typename T, int N>
 void remap(vigra::MultiArray<N, T>& src) {
   T max_value = *vigra::argMax(src.begin(), src.end());
@@ -57,69 +128,113 @@ void remap(vigra::MultiArray<N, T>& src) {
 }
 
 
-// zero padding int to string
-std::string zero_padding(int num, int n_zeros) {
-  std::ostringstream ss;
-  ss << std::setw(n_zeros) << std::setfill('0') << num;
-  return ss.str();
+template <int N>
+void differenceOfGaussians(const vigra::MultiArray<N, unsigned>& src, vigra::MultiArray<N, FEATURETYPE>& dest, double scale0, double scale1, vigra::ConvolutionOptions<N> opt) {
+  vigra::MultiArray<N, FEATURETYPE> tmp(src.shape());
+  vigra::gaussianSmoothMultiArray(srcMultiArrayRange(src), destMultiArray(dest), scale0, opt);
+  vigra::gaussianSmoothMultiArray(srcMultiArrayRange(src), destMultiArray(tmp), scale1, opt);
+  dest -= tmp;
 }
 
 
-// read features from csv_style file
-int read_features_from_file(std::string path, std::vector<std::pair<std::string, std::string> >& features) {
-  std::ifstream f(path.c_str());
-  if (!f.is_open()) return 1;
+template <int N>
+void gaussianGradientMagnitudeOwn(const vigra::MultiArray<N, unsigned>& src, vigra::MultiArray<N, FEATURETYPE>& dest, double scale, vigra::ConvolutionOptions<N> opt) {
+  vigra::MultiArray<N, vigra::TinyVector<FEATURETYPE, N> > tmp(src.shape());
+  vigra::gaussianGradientMultiArray(srcMultiArrayRange(src), destMultiArray(tmp), scale, opt);
+  if (N == 2)
+    vigra::combineTwoMultiArrays(srcMultiArrayRange(tmp),
+				 srcMultiArray(dest),
+				 destMultiArray(dest),
+				 vigra::functor::squaredNorm(vigra::functor::Arg1()));
+  else
+    throw std::runtime_error("gaussianGradientMagnitude only implemented for N == 2 and N == 3");
+  vigra::transformMultiArray(srcMultiArrayRange(dest),
+			     destMultiArray(dest),
+			     vigra::functor::sqrt(vigra::functor::Arg1()));
+  
+}
 
-  typedef boost::tokenizer<boost::escaped_list_separator<char> > Tokenizer;
-  std::string line;
-  while(getline(f, line)) {
-    Tokenizer tok(line);
-    features.push_back(std::make_pair(*tok.begin(), *(++tok.begin())));
+
+template <int N>
+void structureTensorEigenvaluesOwn(const vigra::MultiArray<N, unsigned>& src, std::vector<vigra::MultiArray<N, FEATURETYPE> >& dest, double inner_scale, double outer_scale, vigra::ConvolutionOptions<N> opt) {
+  vigra::MultiArray<N, vigra::TinyVector<FEATURETYPE, (N*(N+1))/2> > tmp1(src.shape());
+  vigra::MultiArray<N, vigra::TinyVector<FEATURETYPE, N> > tmp2(src.shape());
+  vigra::structureTensorMultiArray(srcMultiArrayRange(src), destMultiArray(tmp1), inner_scale, outer_scale, opt);
+  vigra::tensorEigenvaluesMultiArray(srcMultiArrayRange(tmp1), destMultiArray(tmp2));
+  multi_array_of_tiny_vec_to_vec_of_multi_array<N, FEATURETYPE, N>(tmp2, dest);
+}
+
+
+template <int N>
+void hessianOfGaussianEigenvaluesOwn(const vigra::MultiArray<N, unsigned>& src, std::vector<vigra::MultiArray<N, FEATURETYPE> >& dest, double scale, vigra::ConvolutionOptions<N> opt) {
+  vigra::MultiArray<N, vigra::TinyVector<FEATURETYPE, (N*(N+1))/2> > tmp1(src.shape());
+  vigra::MultiArray<N, vigra::TinyVector<FEATURETYPE, N> > tmp2(src.shape());
+  vigra::hessianOfGaussianMultiArray(srcMultiArrayRange(src), destMultiArray(tmp1), scale, opt);
+  vigra::tensorEigenvaluesMultiArray(srcMultiArrayRange(tmp1), destMultiArray(tmp2));
+  multi_array_of_tiny_vec_to_vec_of_multi_array<N, FEATURETYPE, N>(tmp2, dest);
+}
+
+
+template <int N, class T, int U>
+void multi_array_of_tiny_vec_to_vec_of_multi_array(const vigra::MultiArray<N, vigra::TinyVector<T, U> >& src,
+						   std::vector<vigra::MultiArray<N, T> >& dest) {
+  vigra::MultiArrayView<N+1, T, vigra::StridedArrayTag> tmp = src.expandElements(N);
+  for (int i = 0; i < U; ++i) {
+    dest.push_back(tmp.bindOuter(i));
   }
-  f.close();
-  return 0;
+  //vigra::combineTwoMultiArrays(srcMultiArrayRange(src), srcMultiArray(dest), destMultiArray(dest),
 }
 
-// lookup the size of given feature
-int feature_dim_lookup_size(std::string feature) {
-  std::map<std::string, int> dims;
-  dims["GaussianSmoothing"] = 1;
-  dims["LaplacianOfGaussians"] = 1;
-  dims["StructureTensorEigenvalues"] = 3;
-  dims["HessianOfGaussianEigenvalues"] = 3;
-  dims["GaussianGradientMagnitude"] = 1;
-  dims["DifferenceOfGaussians"] = 1;
-  std::map<std::string, int>::iterator it = dims.find(feature);
-  if (it == dims.end()) return 0;
-  else return it->second;
-}
 
-// get features from string
-int get_features(const vigra::MultiArray<2, unsigned>& src, std::vector<vigra::MultiArray<2, double> >& dest, std::string feature, double scale) {
+template <int N>
+int get_features(const vigra::MultiArray<N, unsigned>& src, std::vector<vigra::MultiArray<N, FEATURETYPE> >& dest, std::string feature, double scale) {
   if (dest.size()) return 2;
+  vigra::ConvolutionOptions<N> opt;
+  opt.filterWindowSize(2.0);
   if (!feature.compare("GaussianSmoothing")) {
-    dest.push_back(vigra::MultiArray<2, double>(src.shape()));
-    vigra::gaussianSmoothing(srcImageRange(src), destImage(dest[0]), scale);
+    dest.push_back(vigra::MultiArray<N, FEATURETYPE>(src.shape()));
+    vigra::gaussianSmoothMultiArray(srcMultiArrayRange(src), destMultiArray(dest[0]), scale, opt);
   } else if (!feature.compare("LaplacianOfGaussians")) {
-    dest.push_back(vigra::MultiArray<2, double>(src.shape()));
-    vigra::laplacianOfGaussian(srcImageRange(src), destImage(dest[0]), scale);
+    dest.push_back(vigra::MultiArray<N, FEATURETYPE>(src.shape()));
+    vigra::laplacianOfGaussianMultiArray(srcMultiArrayRange(src), destMultiArray(dest[0]), scale, opt);
   } else if (!feature.compare("StructureTensorEigenvalues")) {
-    for (int i = 0; i < 3; ++i) dest.push_back(vigra::MultiArray<2, double>(src.shape()));
-    vigra::structureTensor(srcImageRange(src), destImage(dest[0]), destImage(dest[1]), destImage(dest[2]), scale, scale);
+    structureTensorEigenvaluesOwn<N>(src, dest, scale, 0.5*scale, opt);
   } else if (!feature.compare("HessianOfGaussianEigenvalues")) {
-    for (int i = 0; i < 3; ++i) dest.push_back(vigra::MultiArray<2, double>(src.shape()));
-    vigra::hessianMatrixOfGaussian(srcImageRange(src), destImage(dest[0]), destImage(dest[1]), destImage(dest[2]), scale);
+    hessianOfGaussianEigenvaluesOwn<N>(src, dest, scale, opt);
   } else if (!feature.compare("GaussianGradientMagnitude")) {
-    dest.push_back(vigra::MultiArray<2, double>(src.shape()));
-    vigra::gaussianGradientMagnitude(srcImageRange(src), destImage(dest[0]), scale);
+    dest.push_back(vigra::MultiArray<N, FEATURETYPE>(src.shape()));
+    gaussianGradientMagnitudeOwn<N>(src, dest[0], scale, opt);
   } else if (!feature.compare("DifferenceOfGaussians")) {
-    dest.push_back(vigra::MultiArray<2, double>(src.shape()));
-    vigra::laplacianOfGaussian(srcImageRange(src), destImage(dest[0]), scale);
+    dest.push_back(vigra::MultiArray<N, FEATURETYPE>(src.shape()));
+    differenceOfGaussians<N>(src, dest[0], scale, 0.66*scale, opt);
   } else return 1;
+  return 0;			     
+}
+
+
+template <int N>
+int get_features(const vigra::MultiArray<N, unsigned>& src, vigra::MultiArrayView<N+1, FEATURETYPE>& dest, std::string feature, double scale, double window_size) {
+  vigra::ConvolutionOptions<N> opt;
+  opt.filterWindowSize(window_size);
+  if (feature.compare("GaussianSmoothing") == 0) {
+    vigra::gaussianSmoothMultiArray(srcMultiArrayRange(src), destMultiArray(dest.bindOuter(0)), scale, opt);
+  } else if (feature.compare("LaplacianOfGaussians") == 0) {
+    vigra::laplacianOfGaussianMultiArray(srcMultiArrayRange(src), destMultiArray(dest.bindOuter(0)), scale, opt);
+  } else if (feature.compare("StructureTensorEigenvalues") == 0) {
+    return 1;
+  } else if (feature.compare("HessianOfGaussianEigenvalues") == 0) {
+    return 1;
+  } else if (feature.compare("GaussianGradientMagnitude") == 0) {
+    gaussianGradientMagnitudeOwn<N>(src, dest.binOuter(0), scale, opt);
+  } else if (feature.compare("DifferenceOfGaussians") == 0) {
+    differenceOfGaussians<N>(src, dest.bindOuter(0), scale, 0.66*scale, opt);
+  } else {
+    return 1;
+  }
   return 0;
 }
 
-// extract number from string
+
 inline double string_to_double(std::string str) {
   std::istringstream is(str);
   double x;
@@ -128,20 +243,6 @@ inline double string_to_double(std::string str) {
   return x;
 }
 
-
-int read_config_from_file(const std::string& path, std::map<std::string, double>& options) {
-  std::ifstream f(path.c_str());
-  if (!f.is_open()) return 1;
-
-  typedef boost::tokenizer<boost::escaped_list_separator<char> > Tokenizer;
-  std::string line;
-  while(getline(f, line)) {
-    Tokenizer tok(line);
-    options[*tok.begin()] = string_to_double(*(++tok.begin()));
-  }
-  f.close();  
-  return 0;
-}
 
 template <int N, class T>
 void renormalize_to_8bit(vigra::MultiArrayView<N, T>& array, double min, double max) {
--- main.cxx
+++ main.cxx
@@ -7,6 +7,9 @@
 #include <utility>
 #include <fstream>
 #include <map>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
 
 // boost
 #include <boost/filesystem.hpp>
@@ -17,6 +20,13 @@
 #include <vigra/random_forest_hdf5_impex.hxx>
 #include <vigra/multi_array.hxx>
 #include <vigra/random_forest.hxx>
+#include <vigra/multi_convolution.hxx>
+#include <vigra/labelimage.hxx>
+#include <vigra/accumulator.hxx>
+
+// pgmlink
+#include <pgmlink/traxels.h>
+#include <pgmlink/tracking.h>
 
 // own
 #include "pipeline_helpers.hxx"
@@ -25,9 +35,17 @@
 // namespaces to be used;
 using namespace std;
 using namespace vigra;
+using namespace vigra::acc;
 namespace fs = boost::filesystem;
 
 typedef unsigned DATATYPE;
+typedef vigra::CoupledIteratorType<2, DATATYPE, DATATYPE>::type Iterator;
+typedef Iterator::value_type Handle;
+typedef AccumulatorChainArray<Handle,
+				   Select<DataArg<1>, LabelArg<2>,
+					  Count, Coord<Mean> >
+			      > chain;
+
 
 int main(int argc, char** argv) {
   // arg 1: dataset folder
@@ -54,6 +72,16 @@ int main(int argc, char** argv) {
       throw runtime_error(tif_dir_str + " is not a directory");
     }
 
+    if (!fs::exists(fs::path(tif_dir_str + "_RES"))) {
+      if (!fs::create_directory(fs::path(tif_dir_str + "_RES"))) {
+	throw runtime_error("Could not create directory: " + tif_dir_str + "_RES");
+      }
+    }
+      
+
+    // int dim = 2;
+
+
     // load segmentation classifier
     string segmentation_ilp_path(dataset_folder + "/segment.ilp");
     vector<RandomForest<unsigned> > rfs;
@@ -70,6 +98,10 @@ int main(int argc, char** argv) {
     vector<pair<string, string> > feature_list;
     int read_status = read_features_from_file(feature_list_path, feature_list);
     if (read_status == 1) throw runtime_error("Could not open file " + feature_list_path);
+    
+    //for (vector<pair<string, string> >::iterator fl_it = feature_list.begin(); fl_it != feature_list.end(); ++fl_it) {
+    //      maxSigma = max(string_to_double(fl_it->second), maxSigma);
+    //}
 
 
     // set config
@@ -84,7 +116,9 @@ int main(int argc, char** argv) {
     // write results to <dataset_folder>/<dataset_sequence_segmentation>
     fs::directory_iterator end_itr;
     vector<MultiArray<2, unsigned> > label_images;
-    for (fs::directory_iterator dir_itr(tif_dir); dir_itr != end_itr; ++dir_itr) {
+    int timestep = 1;
+    pgmlink::TraxelStore ts;
+    for (fs::directory_iterator dir_itr(tif_dir); dir_itr != end_itr; ++dir_itr, ++timestep) {
       string filename(dir_itr->path().string());
       cout << "processing " + filename + " ...\n";
       if (dir_itr->path().extension().string().compare(".tif")) {
@@ -96,59 +130,132 @@ int main(int argc, char** argv) {
       // read image
       ImageImportInfo info(filename.c_str());
       Shape2 shape(info.width(), info.height());
+      MultiArray<2, DATATYPE> src_unsmoothed(shape);
       MultiArray<2, DATATYPE> src(shape);
       MultiArray<2, unsigned> labels(shape);
       
-      importImage(info, destImage(src));
+      importImage(info, destImage(src_unsmoothed));
+      
       // map tp [0,255] if neccessary
       // remap<DATATYPE, 2>(src);
-      renormalize_to_8bit<2, DATATYPE>(src, options["min"], options["max"]);
+      if (options.count("min") && options.count("max"))
+	cout << "normalizing image to [0, 255]\n";
+	renormalize_to_8bit<2, DATATYPE>(src_unsmoothed, options["min"], options["max"]);
 
       // calculate features
       vector<pair<string, string> >::iterator it = feature_list.begin();
-      vector<vector<MultiArray<2, double> > > features;
+      vector<vector<MultiArray<2, FEATURETYPE> > > features;
       int feature_dim = 0;
       for (; it != feature_list.end(); ++it) {
-	cout << " Calculating feature " + it->first + "(s=" + it->second + ")\n";
 	feature_dim += feature_dim_lookup_size(it->first);
-	features.push_back(vector<MultiArray<2, double> >());
+	features.push_back(vector<MultiArray<2, FEATURETYPE> >());
 	double scale = string_to_double(it->second);
-	int feature_status = get_features(src, features[features.size()-1], it->first, scale);
+	double presmooth_sigma = 1.0;
+	if (scale <= 1.0)
+	  presmooth_sigma = scale;
+	else
+	  presmooth_sigma = sqrt(scale*scale - presmooth_sigma*presmooth_sigma);
+	cout << "  Presmoothing image (s=" << it->second << "," << presmooth_sigma << ")\n";
+	vigra::ConvolutionOptions<2> opt;
+	opt.filterWindowSize(3.5);
+	gaussianSmoothMultiArray(srcMultiArrayRange(src_unsmoothed), destMultiArray(src), presmooth_sigma, opt);
+	// gaussianSmoothing(srcImageRange(src_unsmoothed), destImage(src), presmooth_sigma);
+	double feature_sigma = 1.0;
+	if (scale <= 1.0)
+	  feature_sigma = scale;
+	cout << "  Calculating feature " + it->first + "(s=" + it->second + "," << feature_sigma << ")\n";
+	int feature_status = get_features<2>(src, features[features.size()-1], it->first, feature_sigma);
+	for (unsigned index = 0; index < features[features.size()-1].size(); ++index) {
+	  stringstream ss;
+	  ss << features.size() << "_" << index;
+	  string number = ss.str();
+	  cout << "Maximum in Channel " + number + ": " << *argMax(features[features.size()-1][index].begin(), features[features.size()-1][index].end()) << "\n";
+	  exportImage(srcImageRange(features[features.size()-1][index]), ImageExportInfo(("feature" + number + ".png").c_str()));
+	}
+		      
 	if (feature_status == 1)
 	  throw runtime_error("get_features not implemented for feature " + it->first);
 	else if (feature_status == 2)
 	  throw runtime_error("vector passed to get_features is not a zero-length vector");
+	// features[features.size()-1][0] *= 255.0/ *argMax(features[features.size()-1][0].begin(), features[features.size()-1][0].end());
       }
-      MultiArray<2, double> feat(Shape2(1, feature_dim));
-      MultiArray<2, double> res_ar(Shape2(1, 2));
-      for (int x = 0; x < labels.shape()[0]; ++x) {
-	for (int y = 0; y < labels.shape()[1]; ++y) {
-	  int feat_index = 0;
-	  for (vector<vector<MultiArray<2, double> > >::iterator ft_vec_it = features.begin(); ft_vec_it != features.end(); ++ft_vec_it) {
-	    for (vector<MultiArray<2, double> >::iterator ft_it = ft_vec_it->begin(); ft_it != ft_vec_it->end(); ++ft_it, ++feat_index) {
-	      feat(0, feat_index) = (*ft_it)(x, y);
-	    }
+
+
+      
+      MultiArray<2, FEATURETYPE> feat(Shape2(1, feature_dim));
+      MultiArray<2, FEATURETYPE> res_ar(Shape2(1, 2));
+      MultiArray<2, unsigned>::iterator label_it = labels.begin();
+      unsigned step_count(0);
+      for (; label_it != labels.end(); ++label_it, ++step_count) {
+	int feat_index = 0;
+	for (vector<vector<MultiArray<2, FEATURETYPE> > >::iterator ft_vec_it = features.begin(); ft_vec_it != features.end(); ++ft_vec_it) {
+	  for (vector<MultiArray<2, FEATURETYPE> >::iterator ft_it = ft_vec_it->begin(); ft_it != ft_vec_it->end(); ++ft_it, ++feat_index) {
+	    feat(0, feat_index) = *(ft_it->begin()+step_count);
 	  }
-	  double label_pred = 0.0;
-	  for (vector<RandomForest<unsigned> >::iterator rf_it = rfs.begin(); rf_it != rfs.end(); ++rf_it) {
-	    rf_it->predictProbabilities(feat, res_ar);
-	    label_pred += res_ar(0,1);
-	  }
-	  if (label_pred > 0.5*rfs.size())
-	    labels(x,y) = 1;
-	  else
-	    labels(x,y) = 0;
 	}
+	double label_pred_0 = 0.0;
+	double label_pred_1 = 0.0;
+	for (vector<RandomForest<unsigned> >::iterator rf_it = rfs.begin(); rf_it != rfs.end(); ++rf_it) {
+	  rf_it->predictProbabilities(feat, res_ar);
+	  label_pred_0 += res_ar(0,0);
+	  label_pred_1 += res_ar(0,1);
+	}
+	if (label_pred_1 > label_pred_0)
+	  *label_it = 1;
+	else
+	  *label_it = 0;
       }
-      exportImage(srcImageRange(labels), ImageExportInfo((dir_itr->path().filename().string() + ".png").c_str()));
-      // cout << dir_itr->path().filename().string().c_str() << "\n";
+      string segmentation_result_path = tif_dir_str + "_RES/" + dir_itr->path().filename().string() + ".png";
+      exportImage(srcImageRange(labels), ImageExportInfo(segmentation_result_path.c_str()));
+
+      // extract objects
+      MultiArray<2, unsigned> label_image(src.shape());
+      int n_regions = labelImageWithBackground(srcImageRange(labels), destImage(label_image), 1, 0);
+      exportImage(srcImageRange(label_image), ImageExportInfo("labels.png"));
+
+
+      // calculate features and build TraxelStore
+      chain accu_chain;
+      Iterator start = createCoupledIterator(label_image, label_image);
+      Iterator end = start.getEndIterator();
+      extractFeatures(start, end, accu_chain);
+      for (int i = 1; i <= n_regions; ++i) {
+	float size = get<Count>(accu_chain, i);
+	vector<float> com(get<Coord<Mean> >(accu_chain, i).begin(), get<Coord<Mean> >(accu_chain, i).end());
+	if (com.size() == 2)
+	  com.push_back(0);
+	pgmlink::FeatureMap f_map;
+	f_map["com"] = com;
+	f_map["count"].push_back(size);
+	pgmlink::Traxel trax(i, timestep, f_map);
+	pgmlink::add(ts, trax);
+      }
     }
 
-    // extract objects
-
-    // calculate features
-
     // chaingrpah tracking
+    pgmlink::ChaingraphTracking tracker("none",
+					10000., // appearance
+					10000., // disappearance
+					10., // detection
+					500., // misdetection
+					false, // cellness by rf
+					10000., // opportunity cost
+					0., // forbidden cost
+					true, // with constraints
+					false, // fixed detections
+					20., // mean div dist
+					0., // min angle
+					0.05, // ep_gap
+					2., // n neighbors
+					false // alternative builder
+					);
+    
+    vector<vector<pgmlink::Event> > events = tracker(ts);
+    for (vector<vector<pgmlink::Event> >::iterator ts_it = events.begin(); ts_it != events.end(); ++ts_it) {
+      for (vector<pgmlink::Event>::iterator ev_it = ts_it->begin(); ev_it != ts_it->end(); ++ev_it) {
+	cout << *ev_it << "\n";
+      }
+    }					
 
     return 0;
   }
@@ -160,7 +267,7 @@ int main(int argc, char** argv) {
   }
   
   catch (runtime_error& e) {
-    cout << "Program crashed!\n";
+    cout << "Program crashed:\n";
     cout << e.what();
     return 0;
   }
