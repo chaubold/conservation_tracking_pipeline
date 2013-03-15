286995de3829a53750615b6ee3f461574b0e4bad
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2013-03-15 20:23:16 +0100

    changed bash-script

---
+++ SW/N2DL-HeLa-01.sh
@@ -0,0 +1,15 @@
+ds="N2DL-HeLa"
+seq="01"
+
+seqs="01-02"
+
+echo "Segmentation and Tracking for ${ds}-${seq}"
+
+# For each dataset, we trained a classifier for pixelwise segmentation. The 
+# parameters of each classifier and the features used for classification
+# are stored in SW/dependencies/DATASET/*_classifier.h5 and
+# .../*_features.txt, respectively. The parameters needed for 
+# tracking are stored in the same folder as *_config.txt
+
+
+./isbi_pipeline "../${ds}" "${seq}" "dependencies/${ds}/${ds}_${seqs}_config.txt" "dependencies/${ds}/${ds}_${seqs}_classifier.h5" "dependencies/${ds}/${ds}_${seqs}_features.txt"
