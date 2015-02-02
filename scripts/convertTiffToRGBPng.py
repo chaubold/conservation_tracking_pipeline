eb7fada1a9d896b935f099a3e2176f113f8399ad
dstoe <stoeckel.david@gmail.com>
2015-02-02 17:19:40 +0100

    make colorful plots out of the tiff images

---
+++ scripts/convertTiffToRGBPng.py
@@ -0,0 +1,35 @@
+import sys
+import glob
+import os
+import ntpath
+import numpy
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    # get the arguments
+    arguments = sys.argv
+    if len(arguments) < 3:
+        print "usage: {} <input_folder> <output_folder>".format(sys.argv[0])
+        sys.exit(-1)
+    if len(arguments) == 3:
+        normalize = False
+    else:
+        normalize = sys.argv[3]
+    input_folder = sys.argv[1].rstrip('/')
+    output_folder = sys.argv[2].rstrip('/')
+    # loop over all *.tif files
+    for filename in glob.glob("{}/*.tif".format(sys.argv[1])):
+        filename_new = "{}/{}.png".format(
+            output_folder,
+            os.path.splitext(ntpath.basename(filename))[0])
+        print "Convert {} to {}".format(filename, filename_new)
+        # read the file
+        image = plt.imread(filename).astype(numpy.double)
+        if not normalize:
+            if image.max() != 0:
+                normalize = image.max()
+            else:
+                normalize = 1.0
+        # save with new colormap
+        plt.imsave(filename_new, image[:,:,0], vmin=0, vmax=normalize)
+    
