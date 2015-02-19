c916a8f4a49fff381a5e66c7726fd76191d49380
dstoe <stoeckel.david@gmail.com>
2015-02-19 16:20:38 +0100

    shuffling of indices to better distinguish between labels

--- scripts/convertTiffToRGBPng.py
+++ scripts/convertTiffToRGBPng.py
@@ -6,17 +6,19 @@ import numpy
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    # shuffle
+    numpy.random.seed()
     # get the arguments
     arguments = sys.argv
-    if len(arguments) < 3:
-        print "usage: {} <input_folder> <output_folder> [<max id>]".format(sys.argv[0])
+    if len(arguments) < 4:
+        print "usage: {} <input_folder> <output_folder> <max id>".format(sys.argv[0])
         sys.exit(-1)
-    if len(arguments) == 3:
-        normalize = False
-    else:
-        normalize = sys.argv[3]
+    max_id = int(sys.argv[3])
     input_folder = sys.argv[1].rstrip('/')
     output_folder = sys.argv[2].rstrip('/')
+    # get the remapping
+    permutation = numpy.zeros(max_id + 1).astype(numpy.uint8)
+    permutation[1:] = numpy.random.permutation(max_id) + 1
     # loop over all *.tif files
     for filename in glob.glob("{}/*.tif".format(sys.argv[1])):
         filename_new = "{}/{}.png".format(
@@ -24,13 +26,10 @@ if __name__ == "__main__":
             os.path.splitext(ntpath.basename(filename))[0])
         print "Convert {} to {}".format(filename, filename_new)
         # read the file
-        image = plt.imread(filename).astype(numpy.double)
+        image = plt.imread(filename).astype(numpy.uint8)[:,:,0]
         image = numpy.flipud(image)
-        if not normalize:
-            if image.max() != 0:
-                normalize = image.max()
-            else:
-                normalize = 1.0
+        for n in range(1,image.max() + 1, 1):
+            image[image == n] = permutation[n]
         # save with new colormap
-        plt.imsave(filename_new, image[:,:,0], vmin=0, vmax=normalize)
+        plt.imsave(filename_new, image, vmin=0, vmax=max_id)
     
